252eb5bb226f72fedb48f082df2f766535514489
nursofia09 <nur.sofia@mail.unpas.ac.id>
2015-05-04 12:21:31 +0700

    Commit Rkppl

---
+++ Documents/NetBeansProjects/UjianRKPPL/src/main/java/org/ifunpas/rkppl/ujianrkppl/TestCase.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ifunpas.rkppl.ujianrkppl;
+
+/**
+ *
+ * @author SB 604-06
+ */
+public class TestCase {
+    
+    public static void main(String[]args){
+        Soal s = new Soal();
+        s.perkalian(4, 5);
+    }
+    
+}
